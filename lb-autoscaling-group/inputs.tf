0dbf72091b5fc45a3569aa9872cd280fc8777949
tarunkumarpendem <111276921+tarunkumarpendem@users.noreply.github.com>
2022-12-13 20:49:01 +0530

    Add files via upload

---
+++ lb-autoscaling-group/inputs.tf
@@ -0,0 +1,53 @@
+variable "region" {
+  type = string
+}
+
+variable "network_details" {
+  type = object({
+    cidr_block            = string
+    vpc_name              = string
+    subnet_cidrs          = list(string)
+    availability_zones    = list(string)
+    subnet_tags           = list(string)
+    internet_gateway_name = string
+    route_table_name      = string
+    destination_cidr      = string
+    security_group_name   = string
+    protocol              = string
+  })
+}
+
+variable "instances_details" {
+  type = object({
+    ami_id        = string
+    instance_type = string
+    key_pair      = string
+    instance_tags = list(string)
+    launch_template_name = string
+    launch_template_resource_type = string
+    auto_scaling_group_name = string
+    asg_max_size = string
+    asg_min_size = string
+    asg_desired_capacity = string
+    launch_template_tag = string
+  })
+}
+
+variable "load_balancer_details" {
+  type = object({
+    target_group_name  = string
+    target_type        = string
+    load_balancer_type = string
+    load_balancer_name = string
+  })
+}
+
+variable "null_resource_details" {
+  type = object({
+    connection_type = string
+    user_name       = string
+    protocol        = string
+    listener_type   = string
+    listener_name   = string
+  })
+}
